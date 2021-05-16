d949ded4302c7ba70829d08bc20adfc1e8568f71
alex <alex@tekartik.com>
2021-05-16 07:24:12 +0200

    fix: dev lints

--- process_run_example/lib/main.dart
+++ process_run_example/lib/main.dart
@@ -16,30 +16,20 @@ void main() {
 
 class ProcessRunExampleApp extends StatelessWidget {
   // This widget is the root of your application.
+  final ThemeData theme = ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: Colors.lightBlue[800],
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Process run Example',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: Colors.lightBlue[800],
-        accentColor: Colors.cyan[600],
-
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        //primarySwatch: Colors.green,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: theme.copyWith(
+          colorScheme: theme.colorScheme.copyWith(
+        secondary: Colors.cyan[600],
+      )),
       home: MainPage(title: 'Process run example'),
     );
   }
