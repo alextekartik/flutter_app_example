4f1ae0fb5de96e412e0ae0b673c222e9930d488d
Alexandre Roux <alex@tekartik.com>
2024-02-01 11:48:00 +0100

    [sqflite_demo] windows release example

--- demo_sqflite/tool/windows/windows_03_copy_sqlite3_dll.dart
+++ demo_sqflite/tool/windows/windows_03_copy_sqlite3_dll.dart
@@ -1,11 +1,11 @@
 import 'dart:io';
 
 import 'package:path/path.dart';
-import 'package:sqflite_common_ffi/src/windows/setup.dart';
+import 'package:sqflite_common_ffi/windows/sqflite_ffi_setup.dart';
 
 import 'src/helper.dart';
 
 Future<void> main() async {
-  var sqlite3Path = findWindowsDllPath()!;
+  var sqlite3Path = findWindowsSqlite3DllPath()!;
   await File(sqlite3Path).copy(join(windowsBuildPath, basename(sqlite3Path)));
 }
